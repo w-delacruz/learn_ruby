9f6ec070d71814840693c3c99be5e16b07ac9804
Wilbert Delacruz <w.delacruz9@gmail.com>
2017-01-29 12:58:03 -0500

    Finished testing ruby exercises

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
-#write your code here
+def echo(x)
+	x.to_s
+end
+
+def shout(x)
+	x.upcase
+end
+
+def repeat(x, repeats = 2)
+	output = (x + " ") * repeats
+	output.rstrip
+end
+
+def start_of_word(x, letters = 1)
+	letters = letters - 1;
+	x[0..letters]
+end
+
+def first_word(x)
+	x = x.split
+	x[0]
+end
+
+def titleize(x)
+	no_caps = ["and", "over", "the"]
+	x=x.split
+	x.each do |word|
+		word.capitalize! unless no_caps.include?(word)
+	end
+	x[0].capitalize!
+	x.join(" ")
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,14 @@
-#write your code here
+def translate phrase
+  phrase.split.map do |word|
+    word =~ /^     # beginning of string
+    (
+      [^aeiouyq]*  # any number of consonants in a row
+      (qu)?        # or maybe a 'qu'
+    )
+    (.*)           # the rest of the string
+    $/x            # end of string
+    first_phoneme = $1
+    rest_of_word = $3
+    "#{rest_of_word}#{first_phoneme}ay"
+  end.join(" ")
+end
